8815d7d6df7bee381505e3a1d9be7286cc846488
romanzhukovv <zhukov.roman89@gmail.com>
2022-04-16 21:48:31 +0300

    Initial Commit

---
+++ ProfileApp/ProfileAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProfileAppApp.swift
+//  ProfileApp
+//
+//  Created by Roman Zhukov on 16.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct ProfileAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
